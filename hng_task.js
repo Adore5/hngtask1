df33c89b0defd7a1ef24970ba43ea413f2809bbc
Adore5 <83307734+Adore5@users.noreply.github.com>
2024-07-06 06:27:31 +0100

    Add files via upload

---
+++ hng_task.js
@@ -0,0 +1,6 @@
+{
+    {"client ip";"168.0.0.1"} 
+//The "ip address" requester;"location" Lagos//
+//the city of the requester
+"greeting"; "Hello, Julia! the temperature is 11 degrees Celsius in New York
+}
